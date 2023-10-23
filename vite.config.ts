d6e28550d6913f64a3b54b78c40dd20525b528ed
BrianM0330 <BrianM0330@gmail.com>
2023-10-22 20:57:01 -0500

    build: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.js'),
+      entry: resolve(__dirname, 'src/components/index.ts'),
       name: 'lens-widgets-vue',
       fileName: 'lens-widgets-vue'
     }
   },
-  rollupOptions: {
-    external: ["vue", "vue-demi"],
-
-    globals: {
-      vue: "Vue",
-    }
+  optimizeDeps: {
+    exclude: ['vue-demi']
   },
-  plugins: [vue()],
+  rollupOptions: {
+    external: ["vue-demi"],
+    plugins: [
+      peerDepsExternal(),
+      typescript({ include: ['src/components/**/*' ]})
+    ]
+  }
 })
